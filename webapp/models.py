ccc909a9e96791673ff7662f6d163b551b66fc70
Ciro Alberto Gamboa Entralgo <ciro.gamboa.2013@upb.edu.co>
2019-03-16 09:36:53 -0500

    Cambios para Alvi

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+
+#Database tables
+class DoubleCheck(models.Model):
+    code = models.CharField(max_length=6)
+    state = models.ForeignKey(CodeState, on_delete=models.CASCADE)
+    fileid = models.ForeignKey(File, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.code
+
+class File(models.Model):
+    path = models.TextField()
+
+    def __str__(self):
+        return self.path
+
+class CodeState(models.Model):
+    value = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.value
+
+
+
+
