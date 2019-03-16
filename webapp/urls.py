2b44c5879c1b527eea9ac3af31167176a93bfa3e
Ciro Alberto Gamboa Entralgo <ciro.gamboa.2013@upb.edu.co>
2019-03-16 01:03:30 -0500

    incorporacion REST

---
+++ webapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+# from series import views
+
+urlpatterns = [
+#     path('series/', views.serie_list),
+#     path('series/(?P<pk>[0-9]+)/', views.serie_detail),
+    
+]
