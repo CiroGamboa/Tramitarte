c14daac18635130d57fbf0b587ca61229ec76957
Ciro Alberto Gamboa Entralgo <ciro.gamboa.2013@upb.edu.co>
2019-03-15 23:28:59 -0500

    Creacion proyecto en Django

---
+++ tramitserver/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TramitserverConfig(AppConfig):
+    name = 'tramitserver'
---
+++ tramitvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/CiroGamJr/Desktop/Tramitarte/tramitvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
