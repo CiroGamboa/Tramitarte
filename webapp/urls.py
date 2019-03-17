aa556033442dad2f03fcf5186cd3ea1189a3aa38
root <root@ip-172-31-41-215.us-east-2.compute.internal>
2019-03-17 05:37:45 +0000

    setting server

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,6 +5,6 @@ from webapp import views
 urlpatterns = [
     path('home/',views.get_landing,name='getLanding'),
     path('checkCode/<input_code>',views.check_code,name='check_code'),
-    path('.well-known/pki-validation/A8299B98AAF2C5FCD66F150704D6B9A2.txt',views.file_down)
+    #path('.well-known/pki-validation/A8299B98AAF2C5FCD66F150704D6B9A2.txt',views.file_down)
 
 ]
