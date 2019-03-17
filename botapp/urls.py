a625450b13eb51aba9475651395de5ece0d95c2a
Ciro Alberto Gamboa Entralgo <ciro.gamboa.2013@upb.edu.co>
2019-03-17 00:35:37 -0500

    Certificados

--- botapp/urls.py
+++ botapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from botapp import views
 
 urlpatterns = [
-    path('receiveCode/<input_code>',views.receive_code,name='receive_code')
+    path('receiveCode/<input_code>',views.receive_code,name='receive_code'),
+    path('getDoc/',views.get_doc,name='get_doc')
     
 ]
